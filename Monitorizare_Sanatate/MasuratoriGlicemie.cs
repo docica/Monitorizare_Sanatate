153e0ab7c3a34b2c8b0e495102eaca6a750b8cfc
pitud <pitud@ASUS>
2026-03-17 22:25:51 +0200

    Add project files.

---
+++ Monitorizare_Sanatate/MasuratoriGlicemie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monitorizare_Sanatate
+{
+    internal class MasuratoriGlicemie
+    {
+        public DateTime DataMasurare { get; set; }
+        public double ValoareGlicemie { get; set; }
+        public string UnitateMasura { get; set; }
+        public Pacient Pacient { get; set; }
+        public MasuratoriGlicemie(DateTime dataMasurare, double valoareGlicemie, string unitateMasura, Pacient pacient)
+        {
+            DataMasurare = dataMasurare;
+            ValoareGlicemie = valoareGlicemie;
+            UnitateMasura = unitateMasura;
+            Pacient = pacient;
+        }
+        public override string ToString()
+        {
+            return $"Data Masurare: {DataMasurare.ToShortDateString()},Glicemie: {ValoareGlicemie} {UnitateMasura}, Pacient: {Pacient.Nume} {Pacient.Prenume}";
+        }
+    }
+}
